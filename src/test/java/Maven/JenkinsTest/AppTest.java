fe90caf98a845f1bcf960a1722e24173eb0e8541
wesleybortoto <wesleyfontes@gmail.com>
2020-06-08 19:23:36 -0300

    Project created

---
+++ src/test/java/Maven/JenkinsTest/AppTest.java
@@ -0,0 +1,20 @@
+package Maven.JenkinsTest;
+
+import static org.testng.Assert.assertTrue;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
